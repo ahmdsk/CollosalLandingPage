754c8ed3313ae536b31956754af21ba5552f3e52
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-03-08 17:12:20 +0700

    Refactoring Index

--- src/components/CardWrapper.tsx
+++ src/components/CardWrapper.tsx
@@ -1,9 +1,11 @@
-import { ReactNode } from "react"
+import Card from "./Card";
 
-type Props = {
-    children: ReactNode
-}
-
-export default function CardWrapper({ children }: Props) {
-  return <div className="flex gap-5 justify-between pt-10 pb-6">{children}</div>
+export default function CardWrapper() {
+  return (
+    <div className="flex gap-5 justify-between pt-10 pb-6">
+      <Card image="/collosal/icons/figma.svg" title="Design" description="The project interface will be designed first, our favorite tool is Figma." />
+      <Card image="/collosal/icons/code.svg" title="Develop" description="Transform design and write business logic here. Choose the technology you want." />
+      <Card image="/collosal/icons/box.svg" title="Ship" description="After the work is complete, we will send the project and all its assets to you." />
+    </div>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import OurTeam from '@/components/OurTeam'
 import Projects from '@/components/Projects'
 import Services from '@/components/Services'
 import Head from 'next/head'
-import Card from '../components/Card'
 import CardWrapper from '../components/CardWrapper'
 import Header from '../components/Header'
 import HeroSection from '../components/HeroSection'
@@ -21,18 +20,10 @@ export default function Home() {
 
       <Header />
       <HeroSection />
-      
-      <CardWrapper>
-        <Card image="/collosal/icons/figma.svg" title="Design" description="The project interface will be designed first, our favorite tool is Figma." />
-        <Card image="/collosal/icons/code.svg" title="Develop" description="Transform design and write business logic here. Choose the technology you want." />
-        <Card image="/collosal/icons/box.svg" title="Ship" description="After the work is complete, we will send the project and all its assets to you." />
-      </CardWrapper>
-
+      <CardWrapper />
       <BrandLogo />
-
       <WorksSection />
       <OurTeam />
-
       <Projects />
       <Services />
     </>

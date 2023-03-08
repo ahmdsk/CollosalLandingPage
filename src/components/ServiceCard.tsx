e26e3b473409cc790e86df825d8f73471908e25a
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-03-08 17:10:47 +0700

    Refactor Service Pages

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -2,24 +2,26 @@ import Button from "./Button";
 
 interface Props {
     bgClassName: string,
-    textClassName: string
+    textClassName: string,
+    title: 'UI Design' | 'Development' | 'Maintenace',
+    price: 1200 | 5000 | 3000,
+    desc: string[]
 }
 
-export default function ServiceCard({ bgClassName, textClassName }: Props) {
+export default function ServiceCard({ bgClassName, textClassName, title, price, desc }: Props) {
     return (
-        <div className={`rounded-md pt-10 pb-6 px-10 w-3/12 ${bgClassName}`}>
+        <div className={`rounded-md pt-10 pb-6 px-8 w-3/12 ${bgClassName}`}>
             <div className="flex justify-between border-b-2 border-white border-opacity-5 pb-6">
-                <h3 className="font-bold">UI Design</h3>
+                <h3 className="font-bold">{title}</h3>
                 <div className="text-left">
                     <p className="font-light text-xs text-white text-opacity-60 pb-1">Starting from</p>
-                    <h2 className="font-bold text-4xl">1200$</h2>
+                    <h2 className="font-bold text-4xl">{price}$</h2>
                 </div>
             </div>
             <div className="text-center py-6">
-                <p className="pb-4 font-medium">10 design pages</p>
-                <p className="pb-4 font-medium">Well-documented</p>
-                <p className="pb-4 font-medium">4 revisions</p>
-                <p className="pb-4 font-medium">$100/additional page</p>
+                {desc.map((d) => {
+                    return <p className="pb-4 font-medium">{d}</p>
+                })}
             </div>
             <Button text="Detail" className={`bg-white text-gray-700 font-semibold inline-block w-full ${textClassName}`} />
         </div>
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -3,16 +3,47 @@ import ServiceCard from "./ServiceCard";
 export default function Services() {
   return (
     <div className="flex justify-center py-24 bg-[#221048] rounded-lg">
-        <div className="flex flex-col justify-center items-center text-center w-full">
-            <h3 className="text-[#16FCD2] font-semibold text-base">GET STARTED</h3>
-            <h1 className="font-bold text-3xl pt-3 pb-5 w-[500px]">What do you need? Choose a service that can help you</h1>
-            
-            <div className="flex flex-wrap gap-3 justify-center w-full pt-12">
-                <ServiceCard bgClassName="bg-[#3F2379]" textClassName="text-[#3F2379]" />
-                <ServiceCard bgClassName="bg-[#233679]" textClassName="text-[#233679]" />
-                <ServiceCard bgClassName="bg-[#792366]" textClassName="text-[#792366]" />
-            </div>
+      <div className="flex flex-col justify-center items-center text-center w-full">
+        <h3 className="text-[#16FCD2] font-semibold text-base">GET STARTED</h3>
+        <h1 className="font-bold text-3xl pt-3 pb-5 w-[500px]">What do you need? Choose a service that can help you</h1>
+
+        <div className="flex flex-wrap gap-3 justify-center w-full pt-12">
+          <ServiceCard 
+            bgClassName="bg-[#3F2379]" 
+            textClassName="text-[#3F2379]" 
+            title="UI Design" 
+            price={1200} 
+            desc={[
+              '10 design pages',
+              'Well-documented',
+              '4 revisions',
+              '$100/additional page'
+            ]} />
+
+          <ServiceCard 
+            bgClassName="bg-[#233679]" 
+            textClassName="text-[#233679]" 
+            title="Development" 
+            price={5000} desc={[
+              'Web & Mobile',
+              'Well-documented',
+              '8 revisions',
+              '$1000/additional feature'
+            ]} />
+
+          <ServiceCard 
+            bgClassName="bg-[#792366]" 
+            textClassName="text-[#792366]" 
+            title="Maintenace" 
+            price={3000} 
+            desc={[
+              'Daily backup',
+              '3 hourse of maintenance',
+              'Including fixing',
+              '$50/additional hour'
+            ]} />
         </div>
+      </div>
     </div>
   )
 }
